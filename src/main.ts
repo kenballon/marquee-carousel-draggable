05203c2852e057d9bbcde057e6db5a1c259b6662
Ken Ballon <bkenn003@gmail.com>
2023-07-13 16:03:56 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-const parentMarquee = document.querySelector(".marquee-wrapper") as HTMLInputElement;
-const childMarquee = document.querySelector(".marquee-content") as HTMLInputElement;
+const parentMarquee = document.querySelector(".marquee-wrapper") as HTMLDivElement;
+const childMarquee = document.querySelector(".marquee-content") as HTMLDivElement;
 
 // will clone the child node of Parent Marquee or copy the sibling 
-const adChildMarquee= (document.querySelector(".marquee-content") as HTMLInputElement).cloneNode(true);
+const adChildMarquee= (document.querySelector(".marquee-content") as HTMLDivElement).cloneNode(true);
 parentMarquee.appendChild(adChildMarquee);
 
 // code below will allow a draggable feature for the marquee carousel 
-const ulParentListContainer = document.querySelector('.marquee-wrapper') as HTMLInputElement
+const ulParentListContainer = document.querySelector('.marquee-wrapper') as HTMLDivElement
 let isDragging = false;
 
 const dragStart = (e: MouseEvent) => {
